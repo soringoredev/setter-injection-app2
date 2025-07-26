f7b09ed7931792f2b88cb8eb34103dbb996b2383
Sorin Gore <soringoredev@gmail.com>
2025-07-26 10:44:21 +0300

    Configurate of mexican and turkish bean.

---
+++ src/main/java/pixel/academy/setter_injection_app/common/MexicanChef.java
@@ -0,0 +1,8 @@
+package pixel.academy.setter_injection_app.common;
+
+public class MexicanChef implements Chef {
+    @Override
+    public String getDailyRecipe() {
+        return "Mexican food etc etc";
+    }
+}
---
+++ src/main/java/pixel/academy/setter_injection_app/common/TurkishChef.java
@@ -0,0 +1,8 @@
+package pixel.academy.setter_injection_app.common;
+
+public class TurkishChef implements Chef {
+    @Override
+    public String getDailyRecipe() {
+        return "Lulea kebab";
+    }
+}
